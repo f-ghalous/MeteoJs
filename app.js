f7c1c5f7d6e7437004d47a9b0284687f690ebab2
f-ghalous <e2295817@cmaisonneuve.qc.ca>
2023-09-22 18:04:55 -0400

    le programme est fini

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+
+let bouton = document.querySelector('#changer');
 const cle = 'a906eee1b1cae604084a5809aaa73ea3';
-let ville = "tokyo"
-const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ ville +'&appid=' + cle + '&units=metric';
+let ville = "";
+//const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ ville +'&appid=' + cle + '&units=metric';
 //https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=a906eee1b1cae604084a5809aaa73ea3&units=metric
-let requete = new  XMLHttpRequest();
+function recherche(ville){
+ const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ ville +'&appid=' + cle + '&units=metric';
+  let requete = new  XMLHttpRequest();
 requete.open('GET', url);
 requete.responseType = 'json';
 requete.send();
@@ -12,7 +16,7 @@ requete.onload = function() {
             let reponse = requete.response;
             document.querySelector('#ville').textContent = reponse.name;
             document.querySelector('#temperature_label').textContent = reponse.main.temp;
-            console.log(reponse.main.temp);
+            console.log('la reponse ' + reponse.main.temp);
           
           
 
@@ -24,3 +28,11 @@ requete.onload = function() {
     }
     
 }
+  
+}
+
+bouton.addEventListener('click', ()=>{
+    ville = prompt('entrez la ville que vous avez choisie');
+    console.log(ville);
+    recherche(ville);
+})
